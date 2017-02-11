def3b271fa6c12be99f69e6659138c8be00189b1
Lukas <lukas.caup@gmx.de>
2017-02-11 14:27:27 +0100

    created bricks

---
+++ Engine/Brick.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "RectF.h"
+#include "Colors.h"
+#include "Graphics.h"
+
+class Brick
+{
+public:
+    Brick( const RectF& rect_in, Color color_in );
+    void Draw( Graphics& gfx );
+private:
+    RectF rect;
+    Color color;
+    bool destroyed;
+};
