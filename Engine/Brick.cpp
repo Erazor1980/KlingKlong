46b607828ef799e641d7932be451be29109febd5
Lukas <lukas.caup@gmx.de>
2017-02-12 11:33:20 +0100

    fixed inside bounce bug

--- Engine/Brick.cpp
+++ Engine/Brick.cpp
@@ -27,7 +27,13 @@ void Brick::ExecuteBallCollision( Ball& ball )
     assert( CheckBallCollision( ball ) );
 
     const Vec2 ballPos = ball.GetPosition();
-    if( ballPos.x >= rect.left && ballPos.x <= rect.right )
+
+    /* test for "inside" bouncer... */
+    if( std::signbit( ball.GetVelocity().x ) == std::signbit( ( ballPos - GetCenter() ).x ) )
+    {
+        ball.ReboundY();
+    }
+    else if( ballPos.x >= rect.left && ballPos.x <= rect.right )
     {
         ball.ReboundY();
     }
